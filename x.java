1358b9c7de79089743a2db41d3db7f50c4c93106
Jag1603 <120297759+Jag1603@users.noreply.github.com>
2022-12-10 22:45:26 -0800

    Add files via upload

---
+++ x.java
@@ -0,0 +1,33 @@
+package sample;
+
+public class x
+{
+public static void main(String[] args) 
+	{
+	
+	for( int i=0;i<5;i++)
+	{
+		
+		
+		
+		
+	  for(int j=5; j>=1+i; j--)
+	  {
+		  
+		  System.out.print(j);
+		   
+	  }
+
+		System.out.println();
+		
+		
+	} 
+		
+	
+	
+	
+       
+	}	 
+
+
+}
